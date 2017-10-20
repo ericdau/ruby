e6f36f01483735b793edbc23a74545b2f1a0b70b
ericdau <ericdau@gmail.com>
2017-10-20 09:01:21 -0700

    commmmmmit

--- ex25.rb
+++ ex25.rb
@@ -38,9 +38,9 @@ module Ex25
 
     # Sorts the words then prints the first and last one.
     def Ex25.print_first_and_last_sorted(sentence)
-      words = Ex25.sort_sencence(sentence)
+      words = Ex25.sort_sentence(sentence)
       Ex25.print_first_word(words)
       Ex25.print_last_word(words)
     end
 
-  end    
+  end
