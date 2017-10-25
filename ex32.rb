a629a40c9c5ad2e80a1a321ccaf7dd6874185089
ericdau <ericdau@gmail.com>
2017-10-25 16:08:13 -0400

    vccvbnm

--- ex32.rb
+++ ex32.rb
@@ -24,10 +24,10 @@ change.each {|i| puts "I got #{i}" }
 elements =[]
 
 # then use the range operator to do 0 to 5 counts
-(0..5).each.do |i|
-puts "adding #{i} to the list."
-# pushes the i variable on the *end* of the list
-elements.push(i)
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  # pushes the i variable on the *end* of the list
+  elements.push(i)
 end
 
 # now we can print them out too
